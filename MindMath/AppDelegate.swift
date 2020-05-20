1c9125d73ca657ea30d7457cad35b960e2790a91
Joseph Szafarowicz <joeszafarowicz@gmail.com>
2020-05-19 22:59:10 -0400

    StoreKit fix

--- MindMath/AppDelegate.swift
+++ MindMath/AppDelegate.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import SwiftyStoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Complete transactions
+        SwiftyStoreKit.completeTransactions(atomically: true) { purchases in
+            for purchase in purchases {
+                switch purchase.transaction.transactionState {
+                case .purchased, .restored:
+                    if purchase.needsFinishTransaction {
+                        // Deliver content from server, then:
+                        SwiftyStoreKit.finishTransaction(purchase.transaction)
+                    }
+                // Unlock content
+                case .failed, .purchasing, .deferred:
+                    break // do nothing
+                @unknown default:
+                    print("Uknown error")
+                }
+            }
+        }
+        
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-MindMath/Pods-MindMath-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MindMath : NSObject
+@end
+@implementation PodsDummy_Pods_MindMath
+@end
